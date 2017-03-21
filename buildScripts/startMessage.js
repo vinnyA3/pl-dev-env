ec1fea059ba3ea0b11b60455b85a5ca8f068f1af
vinnya3 <vinaceto@yahoo.com>
2017-03-21 16:05:55 -0400

    feat: moved sass to client directory

--- buildScripts/startMessage.js
+++
@@ -1,3 +0,0 @@
-import chalk from "chalk";
-
-console.log(chalk.green('Starting app on dev mode...')); // eslint-disable-line no-console
--- buildScripts/testSetup.js
+++
@@ -1,7 +0,0 @@
-// This file is not transpiled, so must use CommonJS and ES5
-
-// Register babel to transpile before our tests run
-require('babel-register')();
-
-// Disable webpack features that Mocha does not understand
-require.extensions['.css'] = function() {};
