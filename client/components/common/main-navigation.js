4fd0ecfc55844d6fb9665953360396a76c5b32e5
vinnya3 <vinaceto@yahoo.com>
2017-03-22 16:24:25 -0400

    feat: add imports

--- client/components/common/main-navigation.js
+++ client/components/common/main-navigation.js
@@ -6,7 +6,7 @@ export default class MainNavigation extends Component {
   render() {
     return (
       <nav className="main-navi">
-        <div className="main-navi__brand">Swappy</div>
+        <div className="main-navi__brand"><span>Swappy</span></div>
         <ul className="main-navi__links">
           <li className="main-navi__link"><Link to="/signup">Sign Up</Link></li>
           <li className="main-navi__link"><Link to="/signin">Sign In</Link></li>
