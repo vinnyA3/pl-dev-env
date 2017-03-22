d866221a117ea8442ba4420f50e959f65015c0eb
vinnya3 <vinaceto@yahoo.com>
2017-03-22 16:25:05 -0400

    feat: new files

---
+++ client/components/home/home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Hero from './hero';
+import DesktopSection from './desktop-section';
+
+const Home = () => {
+  return (
+    <div className="wrapper">
+      <Hero />
+      <DesktopSection />
+    </div>
+  );
+};
+
+export default Home;
